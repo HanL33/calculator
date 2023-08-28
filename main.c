b9800e8c49ee277866370ea773f0dc6cbda37a33
HanL33 <myla32132@gmail.com>
2023-08-28 19:50:50 +0900

    애매한 :위치 수정

--- main.c
+++ main.c
@@ -14,7 +14,7 @@ int main(int argc, char* argv[])
 	int number1, number2, result;
 	char operator;
 	
-	printf("수식을 입력해주세요 (예> 1 + 2): ");
+	printf("수식을 입력해주세요: (예> 1 + 2) ");
 	scanf("%d %c %d", &number1, &operator, &number2);
 	
 	switch (operator)
@@ -40,21 +40,22 @@ int main(int argc, char* argv[])
  
 int add(int number1, int number2)
 {
-	int num=number1+number2;
-	return num;
+	return number1 + number2;
 }
  
 int sub(int number1, int number2)
 {
-	return number1 - number2;
+	return number1  number2;
 }
  
 int multi(int number1, int number2)
 {
-	return number1 * number2;
+	//TODO implementation
+	return 0;
 }
  
 int div(int number1, int number2)
 {
-	return number1/number2;
+	//TODO implementation
+	return 0;
 }
