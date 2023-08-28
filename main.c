d38e80dd94f660fdc0112d190c6e5b4e71b433db
{minm063} <{jangmin063@gmail.com}>
2023-08-28 03:51:29 -0700

    입력 안내를 변경.

--- main.c
+++ main.c
@@ -14,7 +14,7 @@ int main(int argc, char* argv[])
 	int number1, number2, result;
 	char operator;
 	
-	printf("수식을 입력해주세요 (예> 1 + 2): ");
+	printf("수식을 입력해주세요 : ");
 	scanf("%d %c %d", &number1, &operator, &number2);
 	
 	switch (operator)
